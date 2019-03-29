9aae14107a0c214713268b55e07d2c78e605f267
Joe Biellik <joebiellik@gmail.com>
2019-03-29 20:30:41 +0000

    Remove unused type fields

--- Characters.Shared/Models/Apparel/Component.cs
+++ Characters.Shared/Models/Apparel/Component.cs
@@ -2,7 +2,6 @@ namespace IgiCore.Characters.Shared.Models.Apparel
 {
 	public class Component
 	{
-		public ComponentTypes Type { get; set; }
 		public int Index { get; set; }
 		public int Texture { get; set; }
 	}
--- Characters.Shared/Models/Apparel/Prop.cs
+++ Characters.Shared/Models/Apparel/Prop.cs
@@ -2,7 +2,6 @@ namespace IgiCore.Characters.Shared.Models.Apparel
 {
 	public class Prop
 	{
-		public PropTypes Type { get; set; }
 		public int Index { get; set; }
 		public int Texture { get; set; }
 	}
